8fa4d07426a5d2a36ac1c8f97636f0bf24c78c99
Lily Wang <lilyw357@gmail.com>
2017-07-06 13:54:57 -0400

    243

--- 243.py
+++ 243.py
@@ -1,17 +1,47 @@
-def gcd(a, b): #always with a <= b;
-    if a == 0:
-        return b;
-    return gcd(b%a, a);
+def primes(n):
+    primeslist = [];
+    ind = [1 for i in range(n+1)];
+    for i in range(2, n+1):
+        if ind[i] == 1:
+            for j in range(i*2, n+1, i):
+                ind[j] = 0;
+            primeslist.append(i);
+    return primeslist
 
-curr = 12;
+primeslist = primes(10000);
 
+def totient(n):
+    total = 1;
+    c = n;
+    for p in primeslist:
+        if c%p == 0:
+            count = 0;
+            while c%p == 0:
+                c //= p;
+                count += 1;
+            total *= p**(count-1) * (p - 1)
+        if c == 1:
+            break;
+    if c != 1:
+        return n;
+    return total;
+
+def resilience(n):
+    tot = totient(n);
+    return (tot)/(n-1);
+
+
+curr = 60060;
+rec = 10;
+recnum = 10;
 while 1:
-    numcount = 0;
-    for i in range(1, curr):
-        if gcd(i, curr) == 1:
-            numcount += 1;
-    if numcount*1.0/(curr-1) < 15499.0/94744:
-        print curr
+    res = resilience(curr);
+    if res < rec:
+        #print(res, recnum - res, curr);
+        recnum = curr;
+        rec = res;
+    if res < 15499.0/94744:
+        print(curr)
         break;
-    curr += 1;
+    curr += 60060;
 
